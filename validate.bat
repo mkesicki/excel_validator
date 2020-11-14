f1ef7593f7c085fac666d349a06d5571dbd19418
Michał Kęsicki <marc_he_w@poczta.onet.pl>
2020-11-14 14:25:02 +0100

    Update validate.bat

--- validate.bat
+++ validate.bat
@@ -1,4 +1,4 @@
 echo %1
-python excel_validator.py  "example/example.yml" example\excel.xlsx "Example" "." --errors=true
+python excel_validator.py  "example/example.yml" %1 "Example" %TMP% --errors=true
 pause
 
